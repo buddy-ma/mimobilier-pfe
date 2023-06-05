034300581e703a02aff29dcd00197697099b1354
ayoub iguich <tililayou@gmail.com>
2023-06-05 17:50:32 +0100

    gestion types annonces , gestion annonce et annonce image

--- database/migrations/2023_05_31_074438_create_annonces_table.php
+++ database/migrations/2023_05_31_074438_create_annonces_table.php
@@ -1,43 +1,8 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+/**
+* Reverse the migrations.
+*/
+public function down(): void
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('annonces', function (Blueprint $table) {
-            $table->id();
-            $table->string('Titre',255);
-            $table->integer('type_id');
-            $table->integer('id_promoteur');
-            $table->integer('id_ville');
-            $table->integer('id_quartier');
-            $table->string('Adresse',255);
-            $table->string('extras',255);
-            $table->string('Position',255);
-            $table->string('surface',255);
-            $table->integer('nbr_chambres');
-            $table->biginteger('prix');
-            $table->tinyInteger('Status')->default(1);
-            $table->tinyInteger('is_dispo')->default(1);
-            $table->tinyInteger('is_sponsorised')->default(0);
-            $table->string('vues');
-            $table->timestamps();
-
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('annonces');
-    }
+Schema::dropIfExists('annonces');
+}
 };
